824aea56a9287d7b4f2e0d82f224795683dad2fd
a-n-i-s <>
2025-01-15 11:18:48 +0600

    #feature - added product model -adde tag model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField("name", max_length=50)
+    detail = models.TextField("detail")
+    price = models.DecimalField("price", max_digits=5, decimal_places=2)
+    affiliate_url = models.URLField("affiliate url", max_length=200)
+    image_url = models.URLField("image url", max_length=200)
+    tags = models.ManyToManyField("Tag", verbose_name=("tags"))
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("product-detail", kwargs={"pk": self.pk})
+
+
+class Tag(models.Model):
+    name = models.CharField(name="name", max_length=50)
